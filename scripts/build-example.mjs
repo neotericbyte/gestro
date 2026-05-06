3d1caaa6ce479bf12642933e3976e793852236e9
Firoj Haque <haque.firoj@gmail.com>
2026-05-06 13:11:38 +0530

    Update build for postcard example

--- scripts/build-example.mjs
+++ scripts/build-example.mjs
@@ -5,19 +5,24 @@ import path from "path";
 const examplePath = "example/index.html";
 const distPath = "dist/index.html";
 
-// read file
-let html = fs.readFileSync(examplePath, "utf-8");
+function deployExample(exampleFile) {
+  // read file
+  let html = fs.readFileSync(path.join("examples", exampleFile), "utf-8");
 
-// replace import path
-html = html.replace(
-  'import "../src/gestro-image.js";',
-  'import "./gestro.esm.js";'
-);
+  // replace import path
+  html = html.replace(
+    'import "../src/gestro-image.js";',
+    'import "./gestro.esm.js";',
+  );
 
-// ensure dist exists
-fs.mkdirSync("dist", { recursive: true });
+  // ensure dist exists
+  fs.mkdirSync("dist", { recursive: true });
 
-// write modified file
-fs.writeFileSync(distPath, html);
+  // write modified file
+  fs.writeFileSync(path.join("dist", exampleFile), html);
+}
+
+deployExample("index.html");
+deployExample("postcard.html");
 
 console.log("✔ example/index.html copied and transformed to dist/");
